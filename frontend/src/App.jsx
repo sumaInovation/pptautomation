94388d75d32041ee5e67403c74029363cbf91676
sumaInovation <sumanga0000@gmail.com>
2025-01-24 14:57:51 +0530

    authenicated set

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,51 +1,108 @@
-import React, { useState, useEffect, Children } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { DataProvider } from './context/useTableContext';
-import Analysispage from './pages/Tableanalyspage';
-import Dashbordpage from './pages/Dashbordpage';
-import Loginpage from './pages/Loginpage'
-import Signuppage from './pages/Signuppage';
-import Navbar from './components/common/Navbar'
-import useAuthSrore from './store/useAuthStore'
-import Overviwepage from './pages/Overviwepage'
-import MachineDataForm from './pages/filterdata'
-import Analytics from './pages/Analytics'
-import Technicalreport from './pages/Technicalreportpage'
+import React, { useState, useEffect, Children } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { DataProvider } from "./context/useTableContext";
+import Analysispage from "./pages/Tableanalyspage";
+import Dashbordpage from "./pages/Dashbordpage";
+import Loginpage from "./pages/Loginpage";
+import Signuppage from "./pages/Signuppage";
+import Navbar from "./components/common/Navbar";
+import useAuthSrore from "./store/useAuthStore";
+import Overviwepage from "./pages/Overviwepage";
+import MachineDataForm from "./pages/filterdata";
+import Analytics from "./pages/Analytics";
+import Technicalreport from "./pages/Technicalreportpage";
+import useAuthStore from "./store/useAuthStore";
 
 const App = () => {
-  const{checkAuth}=useAuthSrore()
-//Ever Re-rendering get user details from cooies
-  useEffect(()=>{
-   checkAuth();
-  },[checkAuth])
+  const { user, checkAuth, isAuthtenicted, logout } = useAuthStore();
+  //Ever Re-rendering get user details from cooies
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
 
-
+  // Protected Route Component
+  const PrivateRoute = ({ children }) => {
+    return isAuthtenicted ? children : <Navigate to="/signin" />;
+  };
 
   return (
     <DataProvider>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-
-    
-    <BrowserRouter >
-      <Navbar/>
-    <Routes>
-      <Route path='/' element={<Overviwepage/>}/>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <BrowserRouter>
+          <Navbar />
+          
+            {/* <Route path='/' element={<Overviwepage/>}/>
       <Route path='/login' element={<Loginpage/>}/>
       <Route path='/signup' element={<Signuppage/>}/>
       <Route path='/dashboard' element={<Dashbordpage/>}/>
       <Route path='/report' element={<MachineDataForm/>}/>
       <Route path='/analys' element={<Analysispage/>}/>
       <Route path='/analytics' element={<Analytics/>}/>
-      <Route path='/technical' element={<Technicalreport/>}/>
-    
-    
-    </Routes>
-  
-  </BrowserRouter>
-  </GoogleOAuthProvider>
-  </DataProvider>
-)
-}
+      <Route path='/technical' element={<Technicalreport/>}/> */}
 
-export default App
+            <Routes>
+              {/* Public Routes */}
+              <Route path='/' element={<Overviwepage/>}/>
+              <Route path="/login" element={<Loginpage />} />
+              <Route path="/signup" element={<Signuppage />} />
+
+              {/* Protected Routes */}
+              <Route
+                path="/dashboard"
+                element={
+                  <PrivateRoute>
+                    <Dashbordpage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/report"
+                element={
+                  <PrivateRoute>
+                    <MachineDataForm />
+                  </PrivateRoute>
+                }
+              />
+
+              <Route
+                path="/analys"
+                element={
+                  <PrivateRoute>
+                    <Analysispage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/analytics"
+                element={
+                  <PrivateRoute>
+                    <Analytics />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/technical"
+                element={
+                  <PrivateRoute>
+                    <Technicalreport />
+                  </PrivateRoute>
+                }
+              />
+
+              {/* Default Route */}
+              <Route
+                path="*"
+                element={
+                  <Navigate to={isAuthtenicted ? "/dashboard" : "/login"} />
+                }
+              />
+            </Routes>
+          
+        </BrowserRouter>
+      </GoogleOAuthProvider>
+    </DataProvider>
+  );
+};
+
+export default App;
