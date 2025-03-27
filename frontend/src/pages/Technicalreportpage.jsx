401b3a40544e23dee12199fea24e786f6a8f5a00
Sumanga Wimaladasa <136235586+sumaInovation@users.noreply.github.com>
2025-03-27 10:38:03 +0530

    updates

--- frontend/src/pages/Technicalreportpage.jsx
+++ frontend/src/pages/Technicalreportpage.jsx
@@ -35,9 +35,9 @@ const DynamicReport = () => {
 
      if (elementToUpdate2) {
       if(item[2]==="RUNNING"){
-        elementToUpdate2.runtime+=parseFloat(item[1]).toFixed(2);
+        elementToUpdate2.runtime+=parseFloat(item[1]);
       }else{
-        elementToUpdate2.downtime+=parseFloat(item[1].toFixed(2));
+        elementToUpdate2.downtime+=parseFloat(item[1]);
       }
       
      }
